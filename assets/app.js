8bece8ce23a18bad4862423f5855838a9e74f06f
Anna Kimtis <Anna@Annas-MBP.verizon.net>
2017-01-22 21:28:22 -0500

    added STEAM portfolio page

--- assets/app.js
+++ assets/app.js
@@ -3,3 +3,9 @@ causeRepaintsOn = $("h1, h2, h3, p");
 $(window).resize(function() {
   causeRepaintsOn.css("z-index", 1);
 });
+
+<script>
+  $('.carousel').carousel({
+    interval: 3000
+  })
+</script>
